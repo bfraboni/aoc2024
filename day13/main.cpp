0a07b03aa3f6a3275e852ea3fc560cda258574b1
Basile Fraboni <basile.fraboni@gmail.com>
2024-12-20 08:53:13 -0800

    day13 v2

--- day13/main.cpp
+++ day13/main.cpp
@@ -22,34 +22,10 @@ int main(int argc, char * argv[])
     while(std::getline(file, line))
     {
         if (line.empty()) continue;
-        // std::istringstream iss(line);
-        // if (line.find('A') != std::string::npos)
-        if (sscanf(line.c_str(), "Button A: X+%d, Y+%d", &ax, &ay))
+        else if (sscanf(line.c_str(), "Button A: X+%d, Y+%d", &ax, &ay))
         {
-            // int params = sscanf(line.c_str(), "Button A: X+%d, Y+%d", &ax, &ay);
-            // iss.seekg(12ul); iss >> ax;
-            // const size_t pos = iss.tellg();
-            // iss.seekg(pos+4ul); iss >> ay;
-            std::getline(file, line);
-            sscanf(line.c_str(), "Button B: X+%d, Y+%d", &bx, &by);
-            std::getline(file, line);
-            sscanf(line.c_str(), "Prize: X=%d, Y=%d", &x, &y);
-        // }
-        // // else if (line.find('B') != std::string::npos)
-        // else if (sscanf(line.c_str(), "Button B: X+%d, Y+%d", &bx, &by))
-        // {
-        //     // int params = sscanf(line.c_str(), "Button B: X+%d, Y+%d", &bx, &by);
-        //     // iss.seekg(12ul); iss >> bx;
-        //     // const size_t pos = iss.tellg();
-        //     // iss.seekg(pos+4ul); iss >> by;
-        // }
-        // // else if (line.find('P') != std::string::npos)
-        // else if (sscanf(line.c_str(), "Prize: X=%d, Y=%d", &x, &y))
-        // {
-            // int params = sscanf(line.c_str(), "Prize: X=%d, Y=%d", &x, &y);
-            // iss.seekg(9ul); iss >> x;
-            // const size_t pos = iss.tellg();
-            // iss.seekg(pos+4ul); iss >> y;
+            std::getline(file, line); sscanf(line.c_str(), "Button B: X+%d, Y+%d", &bx, &by);
+            std::getline(file, line); sscanf(line.c_str(), "Prize: X=%d, Y=%d", &x, &y);
 
             // we parsed a full problem, now just solve it
             const int64_t det = (ax * by - ay * bx);
@@ -70,6 +46,5 @@ int main(int argc, char * argv[])
     printf("Data loading time: %f µs\n", t1);
     printf("Part 1: %zu\n", part1);
     printf("Part 2: %zu\n", part2);
-    // printf("Time: %f µs\n", t2);
     return 0;
 }
