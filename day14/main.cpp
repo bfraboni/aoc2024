6f125047e9da4fccc9e2a7b590b1433b9de02aa2
Basile Fraboni <basile.fraboni@gmail.com>
2024-12-22 14:11:44 -0800

    day 14 v2

--- day14/main.cpp
+++ day14/main.cpp
@@ -35,7 +35,6 @@ int main(int argc, char * argv[])
     constexpr int width = 101, height = 103;
     constexpr int halfw = width/2;
     constexpr int halfh = height/2;
-    size_t part1 = 0ul, part2 = 0ul;
     size_t quad[4] = { 0ul };
     while(std::getline(file, line))
     {
@@ -51,8 +50,7 @@ int main(int argc, char * argv[])
             quad[3] += bool(px100 > halfw && py100 < halfh);
         }
     }
-    part1 = quad[0] * quad[1] * quad[2] * quad[3];
-
+    const size_t part1 = quad[0] * quad[1] * quad[2] * quad[3];
     const double t1 = t.micro().count(); t.reset();
 
     int xtime = 0, ytime = 0;
@@ -60,37 +58,30 @@ int main(int argc, char * argv[])
     for (int t = 0; t < 103; ++t)
     {
         int xi[101] = {0}, yi[103] = {0}; // histograms of positions
-        // int nx = 0, ny = 0;
         for(const auto& r : robots)
         {
-            int pxt = mod(r.px + t * r.vx, width);  xi[pxt] += 1;
-            int pyt = mod(r.py + t * r.vy, height); yi[pyt] += 1;
+            const int pxt = mod(r.px + t * r.vx, width);  xi[pxt] += 1;
+            const int pyt = mod(r.py + t * r.vy, height); yi[pyt] += 1;
         }
 
-        // auto var = [](int array[], int size) -> float
-        // {
-        //     float mean = 0, var = 0;
-        //     for(int i = 0; i < size; i++)
-        //     {
-        //         mean += array[i];
-        //         var += array[i] * array[i];
-        //     }
-        //     return std::abs(var / size - mean * mean / (size * size));
-        // };
-        if(t < 101)
+        // find the time with highest variance in Y
+        const float lvx = var(xi, 101);
+        if (t < 101 && lvx > vx)
         {
-            float lvx = var(xi, 101);
-            if (lvx > vx) { vx = lvx; xtime = t; printf("found xtime: %d var %f \n", xtime, lvx); }
-        } 
-        float lvy = var(yi, 103);
-        if (lvy > vy) { vy = lvy; ytime = t; printf("found ytime: %d var %f \n", ytime, lvy); }
-
-        // if (nx >= 2) { xtime = t; printf("found xtime: %d var %f \n", xtime, lvx); }
-        // if (ny >= 2) { ytime = t; printf("found ytime: %d var %f \n", ytime, lvy); }
+            vx = lvx;
+            xtime = t;
+            // printf("found xtime: %d var %f \n", xtime, lvx);
+        }
+        // find the time with highest variance in Y
+        const float lvy = var(yi, 103);
+        if (lvy > vy)
+        {
+            vy = lvy;
+            ytime = t;
+            // printf("found ytime: %d var %f \n", ytime, lvy);
+        }
     }
-
-    part2 = (5253 * xtime + 5151 * ytime) % (width * height);
-
+    const size_t part2 = (5253 * xtime + 5151 * ytime) % (width * height);
     const double t2 = t.micro().count(); t.reset();
 
     printf("Part 1: %zu, Time: %f µs\n", part1, t1);
